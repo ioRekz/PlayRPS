0292737a408658af29676925cb8d2763890d5682
Jeremy Vuillermet <jeremy.vuillermet@gmail.com>
2012-04-26 09:01:07 +0200

    better ws event handling, split javascripts files

---
+++ public/javascripts/app.js
@@ -0,0 +1,108 @@
+$(function() {
+			var refresh
+
+			var socketz = new WSocket(wsURL);
+			socketz.on("newGame", function(data) {
+				$("#p1").html(data.firstPlayer);
+				$("#p2").html(data.secondPlayer).hide().fadeIn("slow");
+				$(".result").html("VERSUSS");
+			});
+			
+			socketz.on("tourneyStart", function(data) {
+				createDomBracket(data.members)
+				$('#players').hide()
+			});
+			
+			socketz.on("result", function(data) {
+				if(data.winner.name == currentUser || data.looser.name == currentUser) {
+					$('.result').show();
+					var left = data.winner.name == currentUser ? data.winner.move : data.looser.move
+					var right = data.winner.name != currentUser ? data.winner.move : data.looser.move
+					$('.result').html("<span>"+ left + " vs " + right +"</span>")
+					$('.result').append("<br/>"+ (data.winner.name == currentUser ? "You win" : "You lost"))
+				}
+				setWinner(data.winner, data.looser, data.round)
+			});
+
+			socketz.on("lost", function(data) {
+				updateAgainst("You lost")
+			});
+
+			socketz.on("youwin", function(data) {
+				$('.result').show();
+				$('.result').html("Gratz, you secured 1st place in @tournament tournament !")
+			});
+
+			socketz.on("join", function(data) {
+				$('#players').append("<li id="+data.user+">"+data.user+"</li>")
+			});
+			
+			socketz.on("infos", function(data) {
+				if(data.results) {
+					console.log(data.results)
+					createDomBracket(data.members)
+					// defaultGame(data.members)
+					_.each(data.results.rounds, function(round, roundNbr) {
+						_.each(round.matches, function(match) {
+							setWinner(match.winner, match.looser, roundNbr)
+						});
+					});
+				 }
+				else {
+				for(i in data.members)
+					$('#players').append("<li id="+data.members[i]+">"+data.members[i]+"</li>")
+				} 
+			});
+
+			socketz.on("tourneyStart", function(data) {
+				createDomBracket(data.members)
+				$('#players').hide()
+			});
+			socketz.on("quit", function(data) {
+				$('#'+data.user).remove()
+				//TODO same id as tree player?
+			});
+			socketz.on("global", function(data) {
+				alert(data.message)
+			});
+
+			var launchTimer = function(data) {
+				var pc = 0;
+				if(refresh)
+				clearInterval(refresh)
+				$('.progress').addClass('active');
+				$('.result').hide();  
+				$('.progress').show();
+				var co = this
+				refresh = setInterval(function() { 
+					pc = pc + 20
+					$('.bar').width(pc + "%")
+					if(pc == 120) {
+						clearInterval(refresh)
+						$('.progress').removeClass('active');
+						$('.progress').hide();
+						$('.result').show();
+						$('.result').html("Too slow ... You Lost")
+						//if(result)
+							
+					}
+				}, 1000)
+				
+			}
+
+			var updateAgainst = function(msg) {
+				$('#against').hide()
+				$('#against')[0].innerText = ""
+				$('#against').append(msg)
+				$('#against').fadeIn('slow')
+			}
+
+			//chatSocket.onmessage = receiveEvent
+			$('.btn').bind('click', function(event) {
+				socketz.send(JSON.stringify({kind: "move", player: currentUser, move: event.currentTarget.value}))
+				clearInterval(refresh)
+				$('.progress').removeClass('active');
+				$('.progress').hide();
+				$('.result').html("And the winner is ...")
+			});
+		});
---
+++ public/javascripts/tournament.js
@@ -0,0 +1,72 @@
+var createDomBracket = function(players) {
+	var mgtop = 8;
+	var offtop = 27;
+	var space = 6;
+	var offspace = 54;
+	var roundid = 0;
+	var roundG = $('#round-template').tmpl({mgtop: mgtop, round:roundid})
+	var round = roundG.find('.nodes')
+	
+	for(i = 0, matchid=0, dir=true; i < players.length; i=i+2, dir= !dir, matchid++){
+		var game = $('#game-template').tmpl({matchid: matchid, direction: dir ? "down" : "up"})
+		var opponentOne = $('#opponent-template').tmpl({name: players[i], nbOpo: "one"})
+		var opponentTwo = $('#opponent-template').tmpl({name: players[i+1], nbOpo: "two"})
+		var gamecontainer = game.find('.match')
+		gamecontainer.append(opponentOne)
+		gamecontainer.append(opponentTwo)
+		round.append(game)
+		var line = $('#line-template').tmpl({height: space, type: dir ? "line" : "space"})
+		round.append(line)
+	}
+	$('.bracket').append(roundG)
+	
+	space = space + offspace;
+	offspace = offspace * 2;
+	mgtop = mgtop + offtop;
+	offtop = offtop * 2
+	roundid++;
+	
+	for(j = players.length/2; j >=2 ; j=j/2, roundid++) {
+		var roundG = $('#round-template').tmpl({mgtop: mgtop, round: roundid})
+		var round = roundG.find('.nodes')
+			for(x = 0, dir=true; x < j/2; x++, dir = !dir) {
+				var opponentOne = $('#opponent-template').tmpl({nbOpo: "one"})
+				var opponentTwo = $('#opponent-template').tmpl({nbOpo: "two"})
+				var gameC = $('#game-template').tmpl({matchid: x,direction: dir ? "down" : "up"});
+				gameC.prepend("<div class='bracket-mark'></div>")
+				var game = gameC.find('.match');
+				game.append(opponentOne)
+				game.append(opponentTwo)
+				round.append(gameC)
+				var line = $('#line-template').tmpl({height: space, type: dir ? "line" : "space"})
+				round.append(line)
+			}
+		space = space + offspace;
+		offspace = offspace * 2;
+		mgtop = mgtop + offtop;
+		offtop = offtop * 2
+		$('.bracket').append(roundG)
+	}
+	$('.bracket-line:last').remove();
+	$('.bracket-down:last').remove();
+}
+
+var setWinner = function(winner, looser, round) {
+	var oppo = $("#round-"+round+" .opponent[data-id='"+winner.name+"']")
+	oppo.addClass('winner')
+	
+	
+	
+	var gameid = parseInt(oppo.parent().attr("data-id"))
+	
+	var nextGameId = Math.floor((gameid)/2)
+	var offsetPlayer = (gameid)%2 == 0 ? "one" : "two"
+	console.log(winner.name)
+	console.log(nextGameId)
+	var nextGame = $("#round-"+(round+1)+" .match[data-id='"+nextGameId+"']")
+	console.log(nextGame)
+	console.log(offsetPlayer)
+	var nextOpo = nextGame.find('.opponent.'+offsetPlayer);
+	nextOpo.append($("#name-template").tmpl({name: winner.name}))
+	nextOpo.attr('data-id', winner.name)
+}
