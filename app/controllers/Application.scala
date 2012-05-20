b675f065f90585c85c6b8f4e127506b353535605
Jeremy Vuillermet <jeremy.vuillermet@gmail.com>
2012-05-20 07:33:43 +0200

    move icons, stream test, update play 20

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,12 @@ import models._
 import models.Tournament
 import play.api.libs.json._
 import models._
+import play.api.libs.iteratee._
+import play.api.libs.Comet
+import akka.actor._
+import play.api.libs.concurrent._
+import play.api.Play.current
+import akka.util.duration._
 
 object Application extends Controller {
 
@@ -26,5 +32,42 @@ object Application extends Controller {
 		
 		Ok(views.html.index(player,tournament))
 	}
+	
+	var clients : List[PushEnumerator[String]] = Nil
   
+	def comet = Action {
+		val enum = Enumerator.imperative[String]()
+		Akka.system.scheduler.scheduleOnce(6 seconds) {
+        enum.push("lol")
+    }
+		Ok.stream(enum &> Comet(callback = "parent.cometMessage"))
+	}
+	
+	def stream = Action {
+		Ok(views.html.stream())
+	}
+	
+	val toEventSource = Enumeratee.map[String]{ msg => 
+		"data: "+msg+"\n\n"}
+
+ val getHeap : Enumerator[String] = Enumerator("kiki")
+
+  def sse = Action {
+		val enum = Enumerator.imperative[String]()
+			Akka.system.scheduler.scheduleOnce(6 seconds) {
+        enum.push("lol")
+				Akka.system.scheduler.scheduleOnce(2 seconds) {
+					enum.push("lol")
+				}
+			}
+   SimpleResult(
+        header = ResponseHeader(OK, Map(
+          CONTENT_LENGTH -> "-1",
+          CONTENT_TYPE -> "text/event-stream"
+        )), 
+        enum &> toEventSource);
+		}
+		
+	
+	
 }
